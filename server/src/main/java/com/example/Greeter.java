1a2b9281f081f11d5eaff52356c6f5ffbd94a432
subbarajuiv <ivsubbaraju@gmail.com>
2017-07-28 16:38:31 +0800

    param added

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
   /**
-  * @param someone
-  * @return greeting
-  */
+   * @param someone the name of a person
+   * @return greeting string
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
